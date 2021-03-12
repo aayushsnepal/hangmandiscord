9a9039c8e9a9d09b3fe64489fccacd053fe548ee
Aayush Nepal <75458803+aayushsnepal@users.noreply.github.com>
2021-03-12 14:12:44 -0500

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -3,8 +3,8 @@ async def hangman(message):
     channel = message.channel
     user = message.author
     r = RandomWords()
-    words = str(r.get_random_word(maxLength=8))
-    wo = words
+    words = (str(r.get_random_word(maxLength=8))).lower()
+    wo = words.lower()
     word = list(words)
     w = ""
     for x in word:
@@ -36,35 +36,35 @@ async def hangman(message):
         await channel.send("Enter a letter: ")
         try:
             guess = await client.wait_for("message", timeout=10)
-            if guess.author == message.author and len(guess.content) != 1 and guess.content != "quit" and \
-                    guess.content != wo:
+            if guess.author == message.author and len(guess.content.lower()) != 1 and guess.content.lower() != "quit" and \
+                    guess.content.lower() != wo:
                 await channel.send("One letter at a time.")
             else:
                 if guess.author == message.author:
                     if guess.content == "quit":
-                        await channel.send("Game exited.")
+                        await channel.send("Game exited. The word was `" + w + "`")
                         return
-                    elif guess.content == wo:
+                    elif guess.content.lower() == wo:
                         gameOver = True
                         await channel.send("You guessed the word! The word was: `" + w + "`")
-                    elif guess.content in word and guess.content not in correctguesses:
+                    elif guess.content.lower() in word and guess.content.lower() not in correctguesses:
                         await channel.send("A correct letter!")
-                        correctguesses.append(guess.content)
+                        correctguesses.append(guess.content.lower())
                         x = 0
                         for i in word:
-                            if i == guess.content:
+                            if i == guess.content.lower():
                                 current[x * 2] = word[x]
                             x += 1
-                    elif guess.content in correctguesses:
-                        await channel.send("`" + guess.content + "` has already been guessed **correctly**!")
+                    elif guess.content.lower() in correctguesses:
+                        await channel.send("`" + guess.content.lower() + "` has already been guessed **correctly**!")
                     elif not gameOver:
-                        if guess.content not in wrongguesses:
-                            wrongguesses.append(guess.content)
+                        if guess.content.lower() not in wrongguesses:
+                            wrongguesses.append(guess.content.lower())
                             attempts -= 1
                             await channel.send("Wrong guess. Attempts left: " + str(attempts))
-                        elif guess.content in wrongguesses:
+                        elif guess.content.lower() in wrongguesses:
                             await channel.send(
-                                "`" + guess.content + "` has already been guessed **incorrectly**! Guess again.")
+                                "`" + guess.content.lower() + "` has already been guessed **incorrectly**! Guess again.")
                     if not gameOver:
                         st = "`"
                         for e in current:
@@ -93,12 +93,11 @@ async def hangman(message):
                             s2 += '`'
                             await channel.send("Wrong guesses: " + s2)
 
-
                 if len(set(word)) == len(correctguesses):
                     gameOver = True
                     await channel.send("You won! The word was: `" + w + "`")
                 if attempts == 0:
                     await channel.send("Out of attempts. The word was: `" + w + "`")
         except:
-            await channel.send("Timed out! The word was: `" + w + "`")
+            await channel.send("Timed out! The word was: " + w)
             gameOver = True
